164f340bf77f0201fd6afbee14f834d775e16dd6
Rodolfo Lima <rodolfo-m16@hotmail.com>
2025-08-19 01:03:57 -0300

    Add lucide-react dependency and refactor components
    - Added lucide-react as a dependency in package.json and package-lock.json.
    - Refactored App component to use Container for layout.
    - Updated Heading component to accept children as props.
    - Enhanced global styles for better layout and design.
    - Created Container component with associated styles for consistent layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import { Heading } from "./components/Heading";
+
+import { Container } from "./components/Container";
 
 export function App() {
   return (
     <>
-      <Heading />
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab cupiditate
-        provident sint obcaecati atque beatae architecto expedita temporibus
-        aspernatur necessitatibus, asperiores ipsa officia nesciunt, mollitia,
-        tempore ratione ex blanditiis dicta?
-      </p>
+      <Container>
+        <section>LOGO</section>
+      </Container>
+      <Container>
+        <section>MENU</section>
+      </Container>
     </>
   );
 }
