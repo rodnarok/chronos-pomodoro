314f65c6c75458d5921c9d7da0f1a1b4a51f0dfd
Rodolfo Lima <rodolfo-m16@hotmail.com>
2025-08-19 23:52:06 -0300

    Add Container and Heading components with styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import "./styles/theme.css";
 import "./styles/global.css";
 
 import { Container } from "./components/Container";
+import { Heading } from "./components/Heading";
 
 export function App() {
   return (
     <>
+      <Heading>LOGO</Heading>
       <Container>
         <section>LOGO</section>
       </Container>
