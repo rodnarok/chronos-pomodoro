52a91a6802117255cdc42fe1d51dd2dd3fca7787
Rodolfo Lima <rodolfo-m16@hotmail.com>
2025-09-25 23:34:28 -0300

    Refactor index.html and Menu component to support dark theme

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,19 +1,43 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
 import styles from "./styles.module.css";
+import { useState } from "react";
+
+type AvailableThemes = "dark" | "light";
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>("dark");
   return (
     <div className={styles.menu}>
-      <a className={styles.menuLink} href="/">
+      <a
+        className={styles.menuLink}
+        href="/"
+        aria-label="Ir para Início"
+        title="Ir para Início"
+      >
         <HouseIcon />
       </a>
-      <a className={styles.menuLink} href="/">
+      <a
+        className={styles.menuLink}
+        href="/"
+        aria-label="Ver Histórico"
+        title="Ver Histórico"
+      >
         <HistoryIcon />
       </a>
-      <a className={styles.menuLink} href="/">
+      <a
+        className={styles.menuLink}
+        href="/"
+        aria-label="Ir para Configurações"
+        title="Ir para Configurações"
+      >
         <SettingsIcon />
       </a>
-      <a className={styles.menuLink} href="/">
+      <a
+        className={styles.menuLink}
+        href="/"
+        aria-label="Mudar tema"
+        title="Mudar tema"
+      >
         <SunIcon />
       </a>
     </div>
